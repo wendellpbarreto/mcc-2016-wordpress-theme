84cb32cd4f0a92ccb963c53e916f69d72b95c473
Wendell P. Barreto <wendellp.barreto@gmail.com>
2015-09-01 22:57:46 -0300

    Stuffs.

--- application/includes/facebook_tags.php
+++ application/includes/facebook_tags.php
@@ -23,5 +23,5 @@
 <meta property="og:url" content="<?php echo $url ?>">
 <meta property="og:type" content="website">
 <meta property="og:title" content="<?php echo $title ?>">
-<meta property="og:description" ontent="<?php echo $description ?>">
+<meta property="og:description" content="<?php echo strip_tags($description) ?>">
 <meta property="og:image" content="<?php echo $image ?>">

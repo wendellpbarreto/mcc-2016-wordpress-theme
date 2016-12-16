4f25d0c60083d3401f659b3b87b71715952ab0f8
Wendell P. Barreto <wendellp.barreto@gmail.com>
2016-12-16 15:08:06 -0300

    New theme.

--- application/footer.php
+++ application/footer.php
@@ -4,9 +4,8 @@
         <script src="<?php echo get_assets_root_uri(); ?>/vendor/classie/classie.js"></script>
 
         <!-- libs@owl-carousel -->
-    	<link rel="stylesheet" href="<?php echo get_assets_root_uri(); ?>/vendor/owl.carousel/assets/owl.carousel.css">
+        <link rel="stylesheet" href="<?php echo get_assets_root_uri(); ?>/vendor/owl.carousel/assets/owl.carousel.css">
         <script src="<?php echo get_assets_root_uri(); ?>/vendor/owl.carousel/owl.carousel.min.js"></script>
-
 
         <?php wp_footer(); ?>
 
@@ -23,11 +22,18 @@
         	fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));</script>
 
-        <!-- twitter api -->
-        <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+        <script>
+        	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        	ga('create', 'UA-89100521-1', 'auto');
+        	ga('send', 'pageview');
+
         </script>
 
         <!-- google maps api -->
         <script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDpeBjRT8swOK43cpc1-NmpdRoTy4eQR9A&sensor=false"></script>
-	</body>
-	</html>
+    </body>
+    </html>
